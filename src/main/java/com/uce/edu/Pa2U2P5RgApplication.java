ff01363cdc0898301c7089e53aaad1370d8da049
ec98 <carlospiruch98@gmail.com>
2023-12-11 19:44:01 -0500

    configuración base de datos

---
+++ src/main/java/com/uce/edu/Pa2U2P5RgApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U2P5RgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U2P5RgApplication.class, args);
+	}
+
+}

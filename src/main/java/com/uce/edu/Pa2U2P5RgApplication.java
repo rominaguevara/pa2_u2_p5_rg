e4002ce4ddfd052abaa5868f839ebe3037ed3104
rominaguevara <rominaguevara2207@hotmail.com>
2024-01-04 23:53:46 -0500

    CRUD ejercicio OneToOne

--- src/main/java/com/uce/edu/Pa2U2P5RgApplication.java
+++ src/main/java/com/uce/edu/Pa2U2P5RgApplication.java
@@ -30,23 +30,35 @@ public class Pa2U2P5RgApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 
-		Ciudadano ciud1 = new Ciudadano();
+		Ciudadano ciuda1 = new Ciudadano();
 	
-		ciud1.setNombre("Romina");
-		ciud1.setApellido("Guevara");
+		ciuda1.setNombre("Romina");
+		ciuda1.setApellido("Guevara");
 		
-		this.iciudadanoService.guardar(ciud1);
+		this.iciudadanoService.guardar(ciuda1);
 		
-		Empleado empl1 = new Empleado();
-		empl1.setFechaIngreso(LocalDateTime.now());
-		empl1.setSalario(new BigDecimal(2000));
+		Empleado emple1 = new Empleado();
+		emple1.setFechaIngreso(LocalDateTime.now());
+		emple1.setSalario(new BigDecimal(2000));
+		
+		Ciudadano ciuda2 =this.iciudadanoService.buscar(31); 
+		
+		emple1.setCiudadano(ciuda2);
+		
+		this.iEmpleadoService.guardar(emple1);
 		
 		
-		Ciudadano ciud2 =this.iciudadanoService.buscar(1); 
+		this.iciudadanoService.eliminar(25);
 		
-		empl1.setCiudadano(ciud2);
+		ciuda1.setApellido("Guevara Gaunuchi");
+		this.iciudadanoService.actualizar(ciuda1);
 		
-		this.iEmpleadoService.guardar(empl1);
+		emple1.setSalario(new BigDecimal(2001));
+		this.iEmpleadoService.actualizar(emple1);
+		
+		this.iEmpleadoService.buscar(2);
+		
+		this.iEmpleadoService.eliminar(19);
 		
 	}
 

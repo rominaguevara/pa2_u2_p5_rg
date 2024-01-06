c0610001d517c33861d700a2a494fdacc7de43b2
rominaguevara <rominaguevara2207@hotmail.com>
2024-01-06 13:38:29 -0500

    Taller 19 version 2

---
+++ src/main/java/com/uce/edu/repository/modelo/Libro2.java
@@ -0,0 +1,65 @@
+package com.uce.edu.repository.modelo;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "libro2")
+public class Libro2 {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_libro2")
+	@SequenceGenerator(name = "seq_libro2", sequenceName = "seq_libro2", allocationSize = 1)
+	@Column(name = "libr_id")
+	private Integer id;
+
+	@Column(name = "libr_titulo")
+	private String titulo;
+
+	@Column(name = "libr_fecha_publicacion")
+	private LocalDateTime fechaPublicacion;
+
+	private List<AutorLibro> autoresLibros; 
+	
+	
+	// GET Y SET
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public LocalDateTime getFechaPublicacion() {
+		return fechaPublicacion;
+	}
+
+	public void setFechaPublicacion(LocalDateTime fechaPublicacion) {
+		this.fechaPublicacion = fechaPublicacion;
+	}
+	
+	public List<AutorLibro> getAutoresLibros() {
+		return autoresLibros;
+	}
+
+	public void setAutoresLibros(List<AutorLibro> autoresLibros) {
+		this.autoresLibros = autoresLibros;
+	}
+}
